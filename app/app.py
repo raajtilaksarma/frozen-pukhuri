97d664857170e81eb5bcc821e79252ff193007d4
raajtilaksarma <raajtilaksarma@gmail.com>
2020-07-20 09:24:33 -0400

    add socket basic structure

--- app/app.py
+++ app/app.py
@@ -1,16 +1,33 @@
 import bot as my_bot
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
-@app.route("/", methods=['POST','GET'])
+# @app.route("/", methods=['POST','GET'])
+# def index():
+#     point_path = []
+#     if request.method == 'POST':
+#         attempts = int(request.form['attempts'])
+#         point_path = my_bot.attempting(attempts)
+#         print(point_path)
+#     return render_template('index.html', point_path = point_path)
+
+@app.route('/')
 def index():
-    point_path = []
-    if request.method == 'POST':
-        attempts = int(request.form['attempts'])
-        point_path = my_bot.attempting(attempts)
-        print(point_path)
-    return render_template('index.html', point_path = point_path)
+    return render_template('index.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__=='__main__':
-    app.run()
+    socketio.run(app, debug=True)
+
+    
